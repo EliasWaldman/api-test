28f3fdd4ca98c4bccd76c83576860274dd826252
Ilia Averin <awers2003@gmail.com>
2025-01-30 02:43:17 +0300

    add base files, first commit

---
+++ database/migrations/2025_01_29_233408_create_tasks_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('tasks', function (Blueprint $table) {
+            $table->id();
+            $table->string('title', 255);
+            $table->text('description')->nullable();
+            $table->dateTime('deadline');
+            $table->dateTime('create_date');
+            $table->string('status')->default('не выполнена');
+            $table->string('priority')->default('низкий');
+            $table->string('category')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('tasks');
+    }
+};
